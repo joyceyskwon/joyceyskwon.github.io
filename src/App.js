64f4658e219db5896760c487e628c0e7f7f5795c
joyceyskwon <joyceyskwon@gmail.com>
2019-05-06 12:26:49 -0400

    added br

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div className="intro">
         <div className="subtext">
-          <p>Welcome to my site! My name is Joyce and I'm a software engineer who <a target="_blank" href="https://www.imjoycekwon.com/">designs</a>. I'm working on some cool <a target="_blank" href="https://github.com/joyceyskwon">projects</a>. Some of the things I like are sparkly socks, synthpop (aka <a target="_blank" href="https://www.youtube.com/watch?v=SseMrNA5grA&list=OLAK5uy_nZQLXfuPRzjtO8ih0mRtUomIk0LSfzvyM">Honne</a>), and inspirational spoken word, all of which you can send to <a href="mailto:joyceyskwon@gmail.com">this address</a>.</p><br></br><p>You might also find me on <a target="_blank" href="https://twitter.com/tinyjoyceee">Twitter</a> or <a target="_blank" href="https://www.youtube.com/channel/UCYl6OOZr2TPb1MvySkokFYw/videos">Youtube</a>.</p>
+          <p>Welcome to my site! My name is Joyce and I'm a software engineer who <a target="_blank" href="https://www.imjoycekwon.com/">designs</a>. I'm working on some cool <a target="_blank" href="https://github.com/joyceyskwon">projects</a>. Some of the things I like are sparkly socks, synthpop (aka <a target="_blank" href="https://www.youtube.com/watch?v=SseMrNA5grA&list=OLAK5uy_nZQLXfuPRzjtO8ih0mRtUomIk0LSfzvyM">Honne</a>), and inspirational spoken word, all of which you can send to <a href="mailto:joyceyskwon@gmail.com">this address</a>.</p><br><p>You might also find me on <a target="_blank" href="https://twitter.com/tinyjoyceee">Twitter</a> or <a target="_blank" href="https://www.youtube.com/channel/UCYl6OOZr2TPb1MvySkokFYw/videos">Youtube</a>.</p>
         </div>
       </div>
     </div>
