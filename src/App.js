14939a89516224afe9b1b08fd717336c287d407b
joyceyskwon <joyceyskwon@gmail.com>
2019-05-06 03:54:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="intro">
+        <div className="subtext">
+          <p>Welcome to my site! My name is Joyce and I'm a software engineer who <a target="_blank" href="https://www.imjoycekwon.com/">designs</a>. I'm working on some cool <a target="_blank" href="https://github.com/joyceyskwon">projects</a>. Some of the things I like are sparkly socks, synthpop (aka <a target="_blank" href="https://www.youtube.com/watch?v=SseMrNA5grA&list=OLAK5uy_nZQLXfuPRzjtO8ih0mRtUomIk0LSfzvyM">Honne</a>), and inspirational spoken word, all of which you can send to <a href="mailto:joyceyskwon@gmail.com">this address</a>. You might also find me on <a target="_blank" href="https://twitter.com/tinyjoyceee">Twitter</a> or <a target="_blank" href="https://www.youtube.com/channel/UCYl6OOZr2TPb1MvySkokFYw/videos">Youtube</a>.</p>
+        </div>
+      </div>
     </div>
   );
 }
